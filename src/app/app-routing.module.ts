82ea78d8e39f6e364d5bef0442baf00f833347f0
Aaron Divina <aaron.divina12@gmail.com>
2022-05-07 01:29:20 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//components
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { TeamComponent } from './team/team.component';
+
+const routes: Routes = [{path:'about', component:AboutComponent},
+                        {path:'home', component:HomeComponent},
+                        {path:'contact', component:ContactComponent},
+                        {path:'team', component:TeamComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
